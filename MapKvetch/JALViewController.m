87ebd9e869b5cc3aff9c14bc431b5bdcafa4e428
Josh Lieberman <josh.lieberman92@gmail.com>
2014-04-01 23:42:28 -0400

    Added Hester St. and Tenement Museum

--- MapKvetch/JALViewController.m
+++ MapKvetch/JALViewController.m
@@ -17,6 +17,16 @@
 #define JTS_LAT 40.812027;
 #define JTS_LON -73.960588;
 
+// Goldsmith coordinates
+
+// 102 Hester St
+#define HESTER_LAT 40.716676;
+#define HESTER_LON -73.993623;
+
+// Tenement Museum
+#define TEN_LAT 40.718767;
+#define TEN_LON -73.990025
+
 // Span
 #define THE_SPAN 0.003f;
 
@@ -48,7 +58,41 @@
     [myMapview setRegion:myRegion animated:YES];
     
     // Annotation
+    NSMutableArray *locations = [[NSMutableArray alloc] init];
+    CLLocationCoordinate2D location;
+    JALAnnotation *myAnn;
     
+    // JTS annotation
+    myAnn = [[JALAnnotation alloc] init];
+    location.latitude = JTS_LAT;
+    location.longitude = JTS_LON;
+    myAnn.coordinate = location;
+    myAnn.title = @"JTS";
+    myAnn.subtitle = @"#welcome2jtsa";
+    [locations addObject:myAnn];
+    
+    // Hester St annotation
+    myAnn = [[JALAnnotation alloc] init];
+    location.latitude = HESTER_LAT;
+    location.longitude = HESTER_LON;
+    myAnn.coordinate = location;
+    myAnn.title = @"102 Hester St";
+    myAnn.subtitle = @"Birthplace of Jacob Epstein";
+    [locations addObject:myAnn];
+    
+    // Tenement Museum annotation
+    myAnn = [[JALAnnotation alloc] init];
+    location.latitude = TEN_LAT;
+    location.longitude = TEN_LON;
+    myAnn.coordinate = location;
+    myAnn.title = @"Tenement Museum";
+    myAnn.subtitle = @"Museum on America's Urban Immigrant History";
+    [locations addObject:myAnn];
+    
+    [self.myMapview addAnnotations:locations];
+    
+    // Old annotation code for single pin
+    /*
     // 1. Create a coordinate for use with the annotation
     CLLocationCoordinate2D jtsLocation;
     jtsLocation.latitude = JTS_LAT;
@@ -60,6 +104,7 @@
     myAnnotation.subtitle = @"The Beisiest Place on Earth!";
     
     [self.myMapview addAnnotation:myAnnotation];
+     */
 }
 
 - (void)didReceiveMemoryWarning
